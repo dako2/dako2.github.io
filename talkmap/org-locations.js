453efff26430fa9745ffec1421f074ce5311f6f0
Ko Da <125230912+Dako2@users.noreply.github.com>
2025-01-08 19:21:43 -0800

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,42 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "San Francisco, CA",
+    37.7749,    // Latitude for San Francisco
+    -122.4194   // Longitude for San Francisco
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Vancouver, Canada",
+    49.2827,    // Latitude for Vancouver
+    -123.1207   // Longitude for Vancouver
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Orlando, Florida",
+    28.5383,    // Latitude for Orlando
+    -81.3792    // Longitude for Orlando
   ],
   [
     "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
-  ]
+    34.0522,    // Latitude for Los Angeles
+    -118.2437   // Longitude for Los Angeles
+  ],
+  [
+    "Honolulu, Hawaii",
+    21.3069,    // Latitude for Honolulu
+    -157.8583   // Longitude for Honolulu
+  ],
+  [
+    "Barcelona, Spain",
+    41.3851,    // Latitude for Barcelona
+    2.1734      // Longitude for Barcelona
+  ],
+  [
+    "Tucson, AZ",
+    32.2226,    // Latitude for Tucson
+    -110.9747   // Longitude for Tucson
+  ],
+  [
+    "Boston, MA",
+    42.3601,    // Latitude for Boston
+    -71.0589    // Longitude for Boston
+  ],
 ];
